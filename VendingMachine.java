47f9eb879e7316aa8aff4e0bd94f2c1a3c788a93
a-r-t <alexthimineur@gmail.com>
2024-02-18 18:38:22 -0500

    user is welcomed to the vending machine, given instructions, and can now select a snack item of their choice

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,18 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class VendingMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+        System.out.println("To use this vending machine, simply select an option, and then enter your money.");
 
         String[] snackNames = new String[]{"Potato Chips", "Pretzels", "Fritos", "Cheez-Its", "Snickers Bar", "Kit Kat Bar", "Oreo Cookies", "Rice Krispy Treat", "Trail Mix", "Popcorn", "Fruit Snacks", "Beef Jerky", "Suspicious Snack"};
         double[] snackPrices = new double[]{1.25, 1.00, 1.25, 1.50, 1.50, 1.25, 1.50, 1.25, 1.00, 1.25, 1.00, 1.50, 1.75};
+
+        System.out.println("The following snacks are currently available:");
 
         for (int i = 0; i < snackNames.length; i++) {
             String formattedPrice = String.format("%.2f", snackPrices[i]);
             System.out.println((i + 1) + ". " + snackNames[i] + " | $" + formattedPrice);
         }
 
-        //Scanner scan = new Scanner(System.in);
+        System.out.println("Please select the number of the snack you would like");
 
-        //tasks accomplished: variety of snacks available for purchase, each snack has a price
+        int selection = -1;
+        do {
+            try {
+                int userInput = scanner.nextInt();
+                if (userInput > 0 && userInput <= snackNames.length) {
+                    selection = userInput - 1;
+                }
+                else {
+                    System.out.println("Error: That number does not correspond with a snack item, please try again.");
+                }
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Error: That is not a valid number, please try again.");
+                scanner.nextLine();
+            }
+        } while (selection < 0);
+        
     }
 }
